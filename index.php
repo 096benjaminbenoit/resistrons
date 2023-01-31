010972908f589e421150aa8fc90d7ad6028f18d7
Benjamin Benoit <benjaminbenoit@macbook-pro-de-benjamin.home>
2023-01-31 14:23:28 +0100

    écriture de l'index

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRONS</title>
+</head>
+<body>
+    <main class="main">
+        <h1 class="main_title">REGISTRONS</h1>
+        <section class="main_infoContainer">
+            <div class="main_infoContainer_left">
+                <h2 class="main_infoContainer_left__title">Résistance :</h2>
+                <p class="main_infoContainer_left__value"></p>
+            </div>
+            <div class="main_infoContainer_right">
+                <h2 class="main_infoContainer_right__title">Tolérance :</h2>
+                <p class="main_infoContainer_right__value"></p>
+            </div>
+        </section>
+        <section class="main_resistanceContainer">
+            <div class="main_resistanceContainer__leftBranch"></div>
+            <div class="main_resistanceContainer_core">
+                <div class="main_resistanceContainer_core__color1"></div>
+                <div class="main_resistanceContainer_core__color2"></div>
+                <div class="main_resistanceContainer_core__color3"></div>
+                <div class="main_resistanceContainer_core__color4"></div>            
+            </div>
+            <div class="main_resistanceContainer__rightBranch"></div>
+        </section>
+            <section class="main_choiseContainer">
+            <select class="main_choiseContainer_color1" name="color1" id="color1-select">
+                <option value="">Sélectionnez une couleur</option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+            <select class="main_choiseContainer_color2" name="color2" id="color2-select">
+                <option value="">Sélectionnez une couleur</option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+            <select class="main_choiseContainer_color3" name="color3" id="color3-select">
+                <option value="">Sélectionnez une couleur</option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+            <select class="main_choiseContainer_color4" name="color4" id="color4-select">
+                <option value="">Sélectionnez une couleur</option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+        </section>
+        <section class="main_valueContainer">
+            <h2 class="main_choiseContainer__title">Valeur de la résistance :</h2>
+            <p class="main_choiseContainer__value"></p>
+        </section>
+    </main>
+</body>
+</html>
