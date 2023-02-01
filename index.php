91b5ec580ef2b0c8e2c9635326b081ef809cd34a
Benjamin Benoit <benjaminbenoit@MacBook-Pro-de-Benjamin.local>
2023-02-01 13:47:09 +0100

    maj

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
+// LES DONNÉES
 $ring1 = array("black"=>0,"brown"=>1,"red"=>2,"orange"=>3,"yellow"=>4,"green"=>5,"blue"=>6,"violet"=>7,"grey"=>8,"white"=>9);
 $ring2 = array("black"=>0,"brown"=>2,"red"=>2,"orange"=>3,"yellow"=>4,"green"=>5,"blue"=>6,"violet"=>7,"grey"=>8,"white"=>9);
 $ring3 = array("black"=>1,"brown"=>10,"red"=>100,"orange"=>1000,"yellow"=>10000,"green"=>100000,"blue"=>1000000,"violet"=>10000000,"grey"=>100000000,"white"=>1000000000,"gold"=>0.1,"silver"=>0.01);
 $ring4 = array("brown"=>1,"red"=>2,"green"=>0.5,"blue"=>0.25,"violet"=>0.10,"grey"=>0.05,"gold"=>5,"silver"=>10);
-?><?php
+
 // RECUPÉRER LE  FORM
 
 $first = $_GET["color1"];
 $second = $_GET["color2"];
 $third = $_GET["color3"];
 $fourth = $_GET["color4"];
+
+// CALCULER LES VALEURS
 
 function calc($first,$second,$third,$fourth){
     $result=0;
@@ -52,7 +55,7 @@ function calc($first,$second,$third,$fourth){
             <section class="main_choiseContainer">
                 <form action="index.php" method="GET">
                     <select class="main_choiseContainer_color1" name="color1" id="color1-select">
-                        <option value="<?php echo $ring1["black"] ?>">NOIR</option>
+                        <option value="<?php echo $ring1["black"] ?>" >NOIR</option>
                         <option value="<?php echo $ring1["brown"] ?>">BRUN</option>
                         <option value="<?php echo $ring1["red"] ?>">ROUGE</option>
                         <option value="<?php echo $ring1["orange"] ?>">ORANGE</option>
