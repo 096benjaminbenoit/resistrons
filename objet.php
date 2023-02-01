24b7cf2d5147a33d27859c289497a3dd2fcf73ad
stephMerlu <stephanie-merluzzo@hotmail.com>
2023-02-01 09:56:04 +0100

    test

---
+++ objet.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    $black = array("anneau1"=>0, "anneau2"=>0, "anneau3"=>1, "anneau4"=>null);
+    $brown = array("anneau1"=>1, "anneau2"=>1, "anneau3"=>10, "anneau4"=>1);
+    $red = array("anneau1"=>2, "anneau2"=>2, "anneau3"=>100, "anneau4"=>2);
+    $orange = array("anneau1"=>3, "anneau2"=>3, "anneau3"=>1000, "anneau4"=>null);
+    $yellow = array("anneau1"=>4, "anneau2"=>4, "anneau3"=>10000, "anneau4"=>null);
+    $green = array("anneau1"=>5, "anneau2"=>5, "anneau3"=>100000, "anneau4"=>0,5);
+    $blue = array("anneau1"=>6, "anneau2"=>6, "anneau3"=>1000000, "anneau4"=>0,25);
+    $violet = array("anneau1"=>7, "anneau2"=>7, "anneau3"=>10000000, "anneau4"=>0,10);
+    $grey = array("anneau1"=>8, "anneau2"=>8, "anneau3"=>100000000, "anneau4"=>0,05);
+    $white = array("anneau1"=>9, "anneau2"=>9, "anneau3"=>1000000000, "anneau4"=>null);
+    $gold = array("anneau1"=>null, "anneau2"=>null, "anneau3"=>0,1, "anneau4"=>5);
+    $silver = array("anneau1"=>null, "anneau2"=>null, "anneau3"=>0,01, "anneau4"=>10);
+
+    
+?>
